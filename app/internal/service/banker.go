e7cae2764740d4525ced4dfdc445ce192c85d480
michael anikammadu <dil.anikamadu@gmail.com>
2026-03-20 10:26:35 +0100

    wallet service

---
+++ app/internal/service/banker.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrInsufficientFunds = errors.New("insufficient funds")
+)
+
+type Banker interface {
+	Deposit(ctx context.Context, walletID int, amount float64, description string) error
+	Withdraw(ctx context.Context, walletID int, amount float64, description string) error
+	Transfer(ctx context.Context, fromWalletID, toWalletID int, amount float64, description string) error
+}
