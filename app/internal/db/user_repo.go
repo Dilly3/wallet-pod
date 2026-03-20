3f94bc6a5d80e959bca46564adee072b023bee80
michael anikammadu <dil.anikamadu@gmail.com>
2026-03-20 10:27:42 +0100

    repo

--- app/internal/db/user_repo.go
+++ app/internal/db/user_repo.go
@@ -12,21 +12,22 @@ type UserRepository struct {
 }
 
 func (ur *UserRepository) CreateUser(ctx context.Context, user *models.User) (int, error) {
-	query := `
-        INSERT INTO users (email, full_name)
-        VALUES ($1, $2)
-        RETURNING id
-    `
+	query := `INSERT INTO users (email, full_name) VALUES ($1, $2) RETURNING id`
 	var id int
 	err := ur.DB.QueryRowContext(ctx, query, user.Email, user.FullName).Scan(&id)
 	return id, err
 }
+
 func (ur *UserRepository) GetUserByID(ctx context.Context, id int) (*models.User, error) {
-	query := `SELECT * FROM users WHERE id = $1`
+	query := `SELECT * FROM users WHERE id = $1 AND deleted_at IS NULL`
 	var user models.User
 	err := ur.DB.GetContext(ctx, &user, query, id)
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
+	return &user, err
+}
+
+func (ur *UserRepository) GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
+	query := `SELECT * FROM users WHERE email = $1 AND deleted_at IS NULL`
+	var user models.User
+	err := ur.DB.GetContext(ctx, &user, query, email)
+	return &user, err
 }
